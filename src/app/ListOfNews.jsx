0eba158346eece9af012a2bc27c43a6dca654d98
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-06-24 13:57:52 -0400

    add env api key

--- src/app/ListOfNews.jsx
+++ src/app/ListOfNews.jsx
@@ -6,8 +6,7 @@ const optionsTranslate = {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
-    "X-RapidAPI-Host":
-      "translated-mymemory---translation-memory.p.rapidapi.com",
+    "X-RapidAPI-Host": process.env.RAPIDAPI_HOST,
   },
 };
 
