0a1ba58b8fd2fab5da27c19ee2ac0513317b3e13
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-06-24 22:11:08 -0400

    no-store

--- src/app/ListOfNews.jsx
+++ src/app/ListOfNews.jsx
@@ -8,7 +8,6 @@ const optionsTranslate = {
     "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
     "X-RapidAPI-Host": process.env.RAPIDAPI_HOST,
   },
-  cache: "no-store",
 };
 
 const url = "https://hltv-api.vercel.app/api/news.json";
